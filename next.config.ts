feba2809cd2c157a392009ed48ce1360edf1bc09
Angel <angel.suarez@maresalogistica.com>
2025-12-15 15:12:12 +0000

    feat: configure Next.js basePath to '/asociacion-juegos-mesa'

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // Necesario si tu repositorio no es tuUsuario.github.io
-  // Descomenta y cambia 'nombre-repo' por el nombre de tu repositorio
-  // basePath: '/nombre-repo',
+  basePath: '/asociacion-juegos-mesa',
 };
 
 export default nextConfig;
